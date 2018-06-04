42b2a79e6529e719eba30268c02ebf3ecf5c0e11
venunath1 <venunathdommeti@gmail.com>
2018-06-04 12:06:36 +0530

    My Third Commit

---
+++ src/com.venu.java/Evenorodd.java
@@ -0,0 +1,13 @@
+package com.venu.java;
+
+public class Evenorodd {
+
+    public static void main(String[] args) {
+        int n=100;
+        if(n%2==0)
+            System.out.println("even number");
+        else
+            System.out.println("odd number");
+
+    }
+}
